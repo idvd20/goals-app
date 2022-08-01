60261ffe8b46edc23d41b283dae75f67ce4e1194
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-08-01 11:38:31 +0800

    Redux practice and frontend update

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,24 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 connectDB();
 
 const app = express();
+
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (!origin || whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
